b23d70e71edd98fbd2ad0cdcf9b4ec06661f044d
sharif37 <sharif.csecu@gmail.com>
2024-12-28 21:24:25 +0600

    implement midpoint circle algorithm

---
+++ src/MidPointCircle.java
@@ -0,0 +1,29 @@
+public class MidPointCircle {
+
+    // Function to draw a circle using the Mid-Point Algorithm
+    public static void drawCircle(int xc, int yc, int r) {
+        int x = 0;
+        int y = r;
+        int p = 1 - r; // Initial decision parameter
+
+        System.out.println("Raster locations (x, y):");
+        BresenhamCircleAlgorithm.plotCirclePoints(xc, yc, x, y);
+
+        while (x < y) {
+            x++;
+            if (p < 0) {
+                p = p + 2 * x + 1; // Move horizontally
+            } else {
+                y--;
+                p = p + 2 * x - 2 * y + 1; // Move diagonally
+            }
+            BresenhamCircleAlgorithm.plotCirclePoints(xc, yc, x, y);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example: Draw a circle with center (0, 0) and radius 5
+        int xc = 0, yc = 0, radius = 10;
+        drawCircle(xc, yc, radius);
+    }
+}
